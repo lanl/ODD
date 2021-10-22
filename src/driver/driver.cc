ce742d391774065206e5ae4fe2bb41b9a6088272
Cleveland <cleveland@lanl.gov>
2021-10-22 11:18:34 -0600

    fix copyright

--- src/driver/driver.cc
+++ src/driver/driver.cc
@@ -4,18 +4,15 @@
  * \author Mathew Cleveland
  * \date   October 21st 2021
  * \brief  Example driver
- * \note   Copyright (C) 2018-2020 Triad National Security, LLC.
- *         All rights reserved. */
+ * \note   Copyright (C) 2021 Triad National Security, LLC., All rights reserved.
+ */
 //------------------------------------------------------------------------------------------------//
 
 #include "solver/opacity_reader.hh"
 #include <iostream>
 
-int main(int argc, char *argv[]) {
-    std::cout<<"Hello World"<<std::endl;
-}
+int main(int argc, char *argv[]) { std::cout << "Hello World" << std::endl; }
 
 //------------------------------------------------------------------------------------------------//
 // end of <basename>.cc
 //------------------------------------------------------------------------------------------------//
-
